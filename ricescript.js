9fabfda682ff53ca94f7ee961c0139678ddbc350
blueymcphluey <josh.mcgee.g@gmail.com>
2020-02-28 15:18:10 +1100

    upgrade rice

--- ricescript.js
+++ ricescript.js
@@ -5,8 +5,10 @@ let containerWidth = 600;
 let slider = document.querySelector('#slider');
 let slider2 = document.querySelector('#slider2');
 
+let currentlyDragging = false;
+
 function moveSlider(e) {
-	if (containerWidth/e.clientY < 5 && containerWidth/e.clientY > 2.4) {
+	if (currentlyDragging && containerWidth/e.clientY < 5 && containerWidth/e.clientY > 2.4) {
 
 		slider.style.cursor = "grabbing";
 		slider.style.transform = `translateX(${0.66*containerWidth}px) translateY(${e.clientY-(0.03) * containerWidth}px)`;
@@ -15,6 +17,12 @@ function moveSlider(e) {
 
 function cancel(e) {
 	slider.style.cursor = "grab";
+	currentlyDragging = false;
+}
+
+function startDragging(e) {
+	currentlyDragging = true;
+	moveSlider(e);
 }
 
 function init() {
@@ -25,6 +33,14 @@ function init() {
 	slider2.style.width = `${(0.07) * containerWidth}px`;
 	slider.style.transform = `translateX(${0.66*containerWidth}px) translateY(${0.3 * containerWidth}px)`;
 	slider2.style.transform = `translateX(${0.79*containerWidth}px) translateY(${0.25 * containerWidth}px)`;
+
+	slider.addEventListener("touchstart", startDragging, false);
+    document.addEventListener("touchend", cancel, false);
+    slider.addEventListener("touchmove", moveSlider, false);
+
+    slider.addEventListener("mousedown", startDragging, false);
+    document.addEventListener("mouseup", cancel, false);
+    slider.addEventListener("mousemove", moveSlider, false);
 
 }
 
