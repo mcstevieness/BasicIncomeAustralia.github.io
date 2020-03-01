dc7bb2a228f2d825284ee31d8648b33d7e6a5d20
blueymcphluey <josh.mcgee.g@gmail.com>
2020-03-01 11:33:33 +1100

    restaurant animation

---
+++ restaurantscript.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// let containerWidth = 200;
+
+function init() {
+	// document.querySelector(":root").style.setProperty('--containerWidth', `${containerWidth}px`);
+}
+
+document.addEventListener("DOMContentLoaded", init);
