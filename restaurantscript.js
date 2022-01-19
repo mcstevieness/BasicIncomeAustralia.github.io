e890860977da0a041a5f7d7ab32dd05c0f829f60
blueymcphluey <josh.mcgee.g@gmail.com>
2022-01-20 03:00:50 +1100

    removing article graphics

--- restaurantscript.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-// let containerWidth = 200;
-
-function init() {
-	// document.querySelector(":root").style.setProperty('--containerWidth', `${containerWidth}px`);
-}
-
-document.addEventListener("DOMContentLoaded", init);
