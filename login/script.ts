f7300d477944dece5ce846c7303bf39b405f3f1a
blueymcphluey <josh.mcgee.g@gmail.com>
2022-01-21 10:07:49 +1100

    first pass at login page

--- login/script.ts
+++ login/script.ts
@@ -8,50 +8,50 @@ async function init() {
 	console.log(AuthHandler);
 	console.log(Jerver);
 
-	// // Your web app's Firebase configuration
-	// const firebaseConfig = {
-	// apiKey: "AIzaSyARIsnJfecYz-qm0UIntCyv6rbuGO80e3M",
-	// authDomain: "joshprojects-b32f5.firebaseapp.com",
-	// projectId: "joshprojects-b32f5",
-	// storageBucket: "joshprojects-b32f5.appspot.com",
-	// messagingSenderId: "608076106886",
-	// appId: "1:608076106886:web:a38b8a427162af374b1507"
-	// };
+	// Your web app's Firebase configuration
+	const firebaseConfig = {
+		apiKey: "AIzaSyAwGEgwBet4EuhaI5X5O5EIdipQXReeC-s",
+		authDomain: "basicincomeaustralia-b98f0.firebaseapp.com",
+		projectId: "basicincomeaustralia-b98f0",
+		storageBucket: "basicincomeaustralia-b98f0.appspot.com",
+		messagingSenderId: "340800003121",
+		appId: "1:340800003121:web:4e9a2a21689310017e0910"
+	};
 
-	// let authHandler = new AuthHandler(firebaseConfig,'#signInContainer',['email','google']);
-	// console.log(authHandler);
+	let authHandler = new AuthHandler(firebaseConfig,'#signInContainer',['email','google']);
+	console.log(authHandler);
 
-	// let logoutButton = document.querySelector('#logoutButton')
-	// logoutButton.addEventListener('click',()=>{
-	// 	authHandler.logout();
-	// })
+	let logoutButton = document.querySelector('#logoutButton')
+	logoutButton.addEventListener('click',()=>{
+		authHandler.logout();
+	})
 
-	// let jerver = new Jerver(authHandler);
+	let jerver = new Jerver(authHandler);
 
-	// jerver.on("message", data => {
-	// 	console.log(`the message is ${data}`);
-	// });
+	jerver.on("message", data => {
+		console.log(`the message is ${data}`);
+	});
 
-	// jerver.on("greetings", (elem) => {
-	// 	console.log(elem);
-	// });
+	jerver.on("greetings", (elem) => {
+		console.log(elem);
+	});
 
-	// jerver.on('goto',(newLocation)=>{
-	// 	console.log(newLocation);
-	// 	// this.location.href = newLocation;
-	// })
+	jerver.on('goto',(newLocation)=>{
+		console.log(newLocation);
+		// this.location.href = newLocation;
+	})
 
-	// jerver.ready();
+	jerver.ready();
 	
-	// jerver.req("salutations", "Howdy!").then((response1)=>{
-	// 	console.log(response1);
-	// });
+	jerver.req("salutations", "Howdy!").then((response1)=>{
+		console.log(response1);
+	});
 
-	// let response2 = await jerver.req("salutations", "Wassup!");
-	// let response3 = await jerver.req("salutations", "Hello!");
+	let response2 = await jerver.req("salutations", "Wassup!");
+	let response3 = await jerver.req("salutations", "Hello!");
 
-	// console.log(response2);
-	// console.log(response3);
+	console.log(response2);
+	console.log(response3);
 
 	
 
