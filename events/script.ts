bd7c7269e04a4d32ad63055f9d6196199e2f12f9
blueymcphluey <josh.mcgee.g@gmail.com>
2022-01-24 06:25:57 +1100

    added events page

---
+++ events/script.ts
@@ -0,0 +1,39 @@
+// @ts-ignore
+import { AuthHandler } from 'https://joshprojects.site/AuthHandler.js';
+// @ts-ignore
+import { Jerver } from "https://joshprojects.site/Jerver.js";
+
+async function init() {
+	// console.log("howdy");
+	// console.log(AuthHandler);
+	// console.log(Jerver);
+
+	// Your web app's Firebase configuration
+	const firebaseConfig = {
+		apiKey: "AIzaSyAwGEgwBet4EuhaI5X5O5EIdipQXReeC-s",
+		authDomain: "basicincomeaustralia-b98f0.firebaseapp.com",
+		projectId: "basicincomeaustralia-b98f0",
+		storageBucket: "basicincomeaustralia-b98f0.appspot.com",
+		messagingSenderId: "340800003121",
+		appId: "1:340800003121:web:4e9a2a21689310017e0910"
+	};
+
+	let authHandler = new AuthHandler(firebaseConfig,'#signInContainer',['email','google']);
+	console.log(authHandler);
+
+	// let logoutButton = document.querySelector('#logoutButton')
+	// logoutButton.addEventListener('click',()=>{
+	// 	authHandler.logout();
+	// })
+
+	let jerver = new Jerver(authHandler);
+
+	jerver.on('goto',(newLocation)=>{
+		console.log(newLocation);
+		// this.location.href = newLocation;
+	})
+
+	jerver.ready();
+}
+
+document.addEventListener("DOMContentLoaded", init);
